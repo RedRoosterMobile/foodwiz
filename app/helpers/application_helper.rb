474fd1e64fe3c4f4e5a3346ecd143d804ab24b07
mojomajor <info@redroostermobile.com>
2014-04-05 19:45:34 +0200

    code from view to app-helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def is_admin?
     current_user.try(:user_level) == 'admin'
   end
+
+  def active_navigation_path(some_path)
+    #fixme: find a better way to do this
+    controller_name == some_path.gsub('/','') ? " class=active" : ''
+  end
 end
